b4016176417d5562d273d1cdaa5eabc9903a18e5
theqasim <kasim7862k@gmail.com>
2023-09-03 23:19:01 +0100

    update

--- src/components/workoutwithchatbot.tsx
+++ src/components/workoutwithchatbot.tsx
@@ -3,15 +3,15 @@ import WorkoutForm from "./form";
 import Chatbot from "./chatbot";
 
 function WorkoutWithChatbot() {
-  const [chatbotMessage, setChatbotMessage] = React.useState<
-    string | JSX.Element[] | null
-  >(null);
+  const [chatbotMessage, setChatbotMessage] = React.useState<string | JSX.Element[] | null>(null);
+
 
   const [convoData, setConvoData] = React.useState<any[]>([]);
 
   const handleFormSubmit = (message: string | JSX.Element[]) => {
     setChatbotMessage(message);
   };
+
 
   return (
     <div className="flex flex-col items-center w-full space-y-6">
