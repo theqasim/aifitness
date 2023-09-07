38f2fe9f93b053806887d23719e759d193add433
theqasim <kasim7862k@gmail.com>
2023-09-07 19:04:09 +0100

    custom goal addition

--- src/components/form.tsx
+++ src/components/form.tsx
@@ -12,6 +12,8 @@ function WorkoutForm(props: WorkoutFormProps) {
   const [showChatbot, setShowChatbot] = useState(false);
   const [loading, setLoading] = useState<boolean>(false);
   const [isValid, setIsValid] = useState(true);
+  const [isCustomGoal, setIsCustomGoal] = useState(false);
+
   const [firstChatbotMessage, setFirstChatbotMessage] = useState<
     string | JSX.Element[] | null
   >(null);
@@ -21,19 +23,19 @@ function WorkoutForm(props: WorkoutFormProps) {
   const [formData, setFormData] = useState({
     gender: "male",
     fitnessGoals: "strength",
-    laggingMuscles: "",
+    targetMuscles: "",
     workoutDays: "",
     weightgoal: "Bulk Up",
   });
 
   const isFormValid = (): boolean => {
-    const { gender, fitnessGoals, laggingMuscles, workoutDays, weightgoal } =
+    const { gender, fitnessGoals, targetMuscles, workoutDays, weightgoal } =
       formData;
 
     return (
       !!gender &&
       !!fitnessGoals &&
-      !!laggingMuscles &&
+      !!targetMuscles &&
       !!workoutDays &&
       !!weightgoal
     );
@@ -42,7 +44,7 @@ function WorkoutForm(props: WorkoutFormProps) {
   const handleChange = (e: { target: { name: string; value: string } }) => {
     const { name, value } = e.target;
 
-    if (name === "laggingMuscles") {
+    if (name === "targetMuscles") {
       const hasNumbers = /\d/.test(value);
       if (hasNumbers) {
         setIsValid(false);
@@ -52,14 +54,26 @@ function WorkoutForm(props: WorkoutFormProps) {
       }
     }
 
-    setFormData((prevData) => ({ ...prevData, [name]: value }));
+    if (name === "fitnessGoals" && value === "custom") {
+      setIsCustomGoal(true);
+    } else if (name === "fitnessGoals") {
+      setIsCustomGoal(false);
+    }
+
+    if (name === "customGoal") {
+      setFormData((prevData) => ({ ...prevData, fitnessGoals: value }));
+    } else {
+      setFormData((prevData) => ({ ...prevData, [name]: value }));
+    }
   };
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
 
+    console.log(formData);
+
     if (!isValid) {
-      alert("Please enter only letters in the laggingMuscles' field.");
+      alert("Please enter only letters in the targetMuscles' field.");
       return;
     }
 
@@ -76,7 +90,7 @@ function WorkoutForm(props: WorkoutFormProps) {
 
       const responseData = await response.json();
 
-      const formattedData = `Gender: ${formData.gender}, Fitness Goals: ${formData.fitnessGoals}, Lagging Muscles: ${formData.laggingMuscles}, Workout Days: ${formData.workoutDays}, Weight Goal: ${formData.weightgoal}`;
+      const formattedData = `Gender: ${formData.gender}, Fitness Goals: ${formData.fitnessGoals}, Target Muscles: ${formData.targetMuscles}, Workout Days: ${formData.workoutDays}, Weight Goal: ${formData.weightgoal}`;
       const convoDataStructure = [
         {
           role: "system",
@@ -152,19 +166,31 @@ function WorkoutForm(props: WorkoutFormProps) {
             >
               <option value="strength">Strength Training</option>
               <option value="muscle">Muscle Building</option>
+              <option value="custom">Custom</option>
             </select>
+            {isCustomGoal && (
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="customGoal"
+                  placeholder="Enter your custom fitness goal"
+                  className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                  onChange={handleChange}
+                />
+              </div>
+            )}
           </div>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="laggingMuscles"
+              htmlFor="targetMuscles"
             >
-              Targetted Muscle Groups:
+              Target Muscle Groups:
             </label>
             <input
-              id="laggingMuscles"
+              id="targetMuscles"
               type="text"
-              name="laggingMuscles"
+              name="targetMuscles"
               onChange={handleChange}
               placeholder="e.g. biceps, calves"
               className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
@@ -175,23 +201,6 @@ function WorkoutForm(props: WorkoutFormProps) {
               Please enter only what is requested.
             </p>
           )}
-          <div className="mb-4">
-            <label
-              className="block text-gray-700 text-sm font-bold mb-2"
-              htmlFor="workoutDays"
-            >
-              How many days a week do you want to workout?
-            </label>
-            <input
-              id="workoutDays"
-              type="number"
-              name="workoutDays"
-              min="1"
-              max="7"
-              onChange={handleChange}
-              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-            />
-          </div>
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -209,6 +218,23 @@ function WorkoutForm(props: WorkoutFormProps) {
               <option value="lean">Lean Out</option>
               <option value="nopref">N/A</option>
             </select>
+          </div>
+          <div className="mb-4">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="workoutDays"
+            >
+              How many days a week do you want to workout?
+            </label>
+            <input
+              id="workoutDays"
+              type="number"
+              name="workoutDays"
+              min="1"
+              max="7"
+              onChange={handleChange}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+            />
           </div>
           <div className="flex items-center justify-center">
             {loading && <Loadinganimation />}
